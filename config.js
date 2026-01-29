a400ced5d560217d17bc9344903ffbf61b5f3555
ahmadhaseb <haseebahmad.iiui@gmail.com>
2026-01-29 19:29:05 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// TaxMate Configuration - Private File
+window.APP_CONFIG = {
+    // n8n Production URL
+    WEBHOOK_URL: "https://shiningstarplay321.app.n8n.cloud/webhook/fcfeee50-f112-474c-9176-ff392d93a8a6",
+    
+};
